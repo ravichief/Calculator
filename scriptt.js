9d5fd4253c1dae5ebab54a8db9db7517a2998d85
RAVI SHANKAR SUMAN <95693410+ravichief@users.noreply.github.com>
2023-03-07 13:09:13 +0530

    Update scriptt.js

--- scriptt.js
+++ scriptt.js
@@ -2,6 +2,8 @@ const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('.button');
 let operand1 = null;
 let operator = null;
+let result = null;
+
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     const value = button.value;
@@ -9,7 +11,12 @@ buttons.forEach(button => {
       clearDisplay();
     } 
     else if (value === '+' || value === '-' || value === '*' || value === '/') {
-      operand1 = parseFloat(display.textContent);
+      if (result !== null) {
+        operand1 = result;
+        result = null;
+      } else {
+        operand1 = parseFloat(display.textContent);
+      }
       operator = value;
       display.textContent = '';
     } 
@@ -17,37 +24,46 @@ buttons.forEach(button => {
       handleEquals();
     } 
     else {
+      if (result !== null) {
+        clearDisplay();
+        result = null;
+      }
       appendValue(value);
     }
   });
 });
+
 function appendValue(value) {
   display.textContent += value;
 }
+
 function clearDisplay() {
   display.textContent = '';
   operand1 = null;
   operator = null;
+  result = null;
 }
+
 function handleEquals() {
   if (!operator) {
     return;
   }
   const operand2 = parseFloat(display.textContent);
-  const result = calculateResult(operand1, operator, operand2);
+  switch (operator) {
+    case '+':
+      result = operand1 + operand2;
+      break;
+    case '-':
+      result = operand1 - operand2;
+      break;
+    case '*':
+      result = operand1 * operand2;
+      break;
+    case '/':
+      result = operand1 / operand2;
+      break;
+  }
   display.textContent = result;
   operand1 = null;
   operator = null;
-}
-function calculateResult(operand1, operator, operand2) {
-  switch (operator) {
-    case '+':
-      return operand1 + operand2;
-    case '-':
-      return operand1 - operand2;
-    case '*':
-      return operand1 * operand2;
-    case '/':
-      return operand1 / operand2;
-  }
 }
