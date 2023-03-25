fe3205ee67a33a702eccefc2f01bab6a4448edbd
RAVI SHANKAR SUMAN <95693410+ravichief@users.noreply.github.com>
2023-03-26 02:18:34 +0530

    Update scriptt.js
    Updated the bug where the operands where operated continuously until the user entered "=".

--- scriptt.js
+++ scriptt.js
@@ -1,66 +1,60 @@
 const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('.button');
-let operand1 = null;
-let operator = null;
-let result = null;
+let operands = [];
+let operators = [];
+
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     const value = button.value;
     if (value === 'C') {
       clearDisplay();
-    } 
+    }
     else if (value === '+' || value === '-' || value === '*' || value === '/') {
-      if (result !== null) {
-        operand1 = result;
-        result = null;
-      } else {
-        operand1 = parseFloat(display.textContent);
-      }
-      operator = value;
+      operators.push(value);
+      operands.push(parseFloat(display.textContent));
       display.textContent = '';
-    } 
+    }
     else if (value === '=') {
+      operands.push(parseFloat(display.textContent));
       handleEquals();
-    } 
+    }
     else {
-      if (result !== null) {
-        clearDisplay();
-        result = null;
-      }
       appendValue(value);
     }
   });
 });
+
 function appendValue(value) {
   display.textContent += value;
 }
 
 function clearDisplay() {
   display.textContent = '';
-  operand1 = null;
-  operator = null;
-  result = null;
+  operands = [];
+  operators = [];
 }
+
 function handleEquals() {
-  if (!operator) {
-    return;
-  }
-  const operand2 = parseFloat(display.textContent);
-  switch (operator) {
-    case '+':
-      result = operand1 + operand2;
-      break;
-    case '-':
-      result = operand1 - operand2;
-      break;
-    case '*':
-      result = operand1 * operand2;
-      break;
-    case '/':
-      result = operand1 / operand2;
-      break;
+  let result = operands[0];
+  for (let i = 0; i < operators.length; i++) {
+    const operator = operators[i];
+    const operand = operands[i + 1];
+    switch (operator) {
+      case '+':
+        result += operand;
+        break;
+      case '-':
+        result -= operand;
+        break;
+      case '*':
+        result *= operand;
+        break;
+      case '/':
+        result /= operand;
+        break;
+    }
   }
   display.textContent = result;
-  operand1 = null;
-  operator = null;
+  operands = [result];
+  operators = [];
 }
